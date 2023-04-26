26b2785be300ee4db06503b0010418c5ba282a11
alan <alan.softdev@gmail.com>
2023-04-26 10:46:42 +0700

    render html

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "0500e1ca28a210632d8bf3a862fcee0a",
-"index.html": "e1a99f11d327fa149c34bfa9ea5e30e7",
-"/": "e1a99f11d327fa149c34bfa9ea5e30e7",
-"main.dart.js": "99827ae8760c7f0bc567f9ac194d5c43",
+"index.html": "15e75d589c2e3e46a272622a4ffe0ee4",
+"/": "15e75d589c2e3e46a272622a4ffe0ee4",
+"main.dart.js": "4e89a813bd8bb6d2a8a6fec25b795d7e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
